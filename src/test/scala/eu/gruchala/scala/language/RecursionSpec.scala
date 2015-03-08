78ead5199de5e3619f0184f0cd33c02295920b75
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-03-08 12:20:48 +0100

    options & recusrion & variables

---
+++ src/test/scala/eu/gruchala/scala/language/RecursionSpec.scala
@@ -0,0 +1,56 @@
+package eu.gruchala.scala.language
+
+import org.scalatest._
+import org.scalatest.Matchers._
+import org.scalatest.{GivenWhenThen, FunSpec}
+
+import scala.annotation.tailrec
+
+class RecursionSpec extends FunSpec with GivenWhenThen {
+
+  describe("Recursion") {
+    it("is a powerful concept") {
+      ""
+    }
+
+    it("is a cornerstone of functional programming") {
+      "Functional languages as Haskell, Clojure, Schema and many more use recursion as a primary way of doing looping".contains("recursion") shouldBe true
+    }
+
+    it("in scala requires a return a return type") {
+      //function return types might be inferred, but in case of recursion it might lead to a infinite loop
+      def length(l: List[String]): Int = l match {
+        case Nil => 0 // recursion base case, zero elements
+        case head :: rest => 1 + length(rest) // method is calling itself, at least one element
+      }
+
+      length("Recursion consist of two cases - base case when we should end and recursive step".split(" ").toList) shouldBe 15
+    }
+
+    it("might be very fast if you use tail recursion optimization, to ensure that mark your recursive functions with @tailrec") {
+      def length(l: List[String]): Int = {
+        @tailrec
+        def internal(ll: List[String], counter: Int): Int = ll match {
+          case Nil => counter
+          case head :: rest => internal(rest, counter + 1)
+        }
+        internal(l, 0)
+      }
+
+      length("Method is tail recursive when last call is recursion or returning value".split(" ").toList) shouldBe 12
+    }
+
+    // Exercises: explore on your own
+    // Add a correct implementation to the `transform` method, remove 'pending' and make those tests green!
+    pending
+    it("allows us to express every looping function") {
+      def transform(l: List[Int], f: (Int) => Int) = l
+
+      val double = (x: Int) => x*2
+      transform(List(1,2,3), double) shouldBe List(2,4,6)
+
+      val increment = (x: Int) => x+1
+      transform(List(-1,0,1), increment) shouldBe List(0,1,2)
+    }
+  }
+}
--- src/test/scala/eu/gruchala/scala/language/VariablesSpec.scala
+++ src/test/scala/eu/gruchala/scala/language/VariablesSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest._
 import org.scalatest.Matchers._
 import org.scalatest.{GivenWhenThen, FunSpec}
 
-class Variables extends FunSpec with GivenWhenThen {
+class VariablesSpec extends FunSpec with GivenWhenThen {
 
   describe("Variables in Scala") {
     it("come in two flavours - immutable values, created with val keyword"){
@@ -25,7 +25,7 @@ class Variables extends FunSpec with GivenWhenThen {
     }
 
     it("don't need to put a type since it's inferred, but you can add it to improve readability and compile times") {
-      def returnString(): String = "Let's return a String to satisfy the type requirement"
+      def returnString(): String = "Let's return a String to satisfy the tlazy val imLazy = new Date()ype requirement"
       def returnTypesCanBeInferredToo() = "It's inferred as String"
 
       val implicitType: String = "I'm a String and you can know it"
@@ -42,9 +42,21 @@ class Variables extends FunSpec with GivenWhenThen {
     }
 
     it("can deferred until really needed - we call them lazy") {
-      lazy val imLazy = new Date
-      Thread.sleep(1000)
+      lazy val imLazy = new Date()
+      Thread.sleep(2000)
       val imEager = new Date()
+      Thread.sleep(2000)
+
+      imEager.before(imLazy) shouldBe true // we declared it before, but the date is more recent
+    }
+
+    it("can also hold functions") {
+      def namedFunction() = { "Hi, I'm named function!" }
+      val namedFunctionHandle = namedFunction _
+      val anonymousFunctionHandle = () => { "Hi, I'm anonymous!" }
+
+      namedFunction() shouldBe "Hi, I'm named function!"
+      anonymousFunctionHandle() shouldBe "Hi, I'm anonymous!"
     }
   }
 }
