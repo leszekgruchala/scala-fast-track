e5e84ca40ab653031814f30f03ae2bf25005df74
Leszek Gruchala <leszek@gruchala.eu>
2017-10-12 22:52:40 +0200

    Simplified a lot variances

--- src/main/scala/eu/gruchala/scala/language/variances/package.scala
+++ src/main/scala/eu/gruchala/scala/language/variances/package.scala
@@ -6,74 +6,36 @@ package object variances {
   trait Parent extends Human
   trait Child extends Parent
 
-  object invariance {
+  trait invariance {
 
-    //invariant, can return only type of A
-    class Box[A] {
+    //invariant - Bus can take only A type, nothing else
+    class Bus[A] {}
+    def drive(a: Bus[Parent]): Unit
 
-      def set(a: A): Box[A] = new Box[A]
-    }
-
-    val parent: Box[Parent] = new Box[Parent]
-
-    //Not allowed, because Child is a subtype of Parent
-    //  val parentToChild: Box[Child] = parent
-    //  val child: Box[Child] = parent.set(new Child {})
-
-    //Not allowed, because Human is a supertype of Parent
-    //  val parentToHuman: Box[Human] = parent
-    //  val human: Box[Human] = parent.set(new Human {})
-
-    //Allowed, because Parent is a type of Parent
-    val parentToParent: Box[Parent] = parent
-    val anotherParent: Box[Parent] = parent.set(new Parent {})
+    //drive(new Bus[Human])
+    drive(new Bus[Parent])
+    //drive(new Bus[Child])
   }
 
-  object contravariance {
+  trait contravariance {
 
-    //contravariant, can result in type of A and its subtypes
-    class Box[-A] {
+    //contravariant - can take type of A and its supertypes
+    class Bus[-A] {}
+    def drive(a: Bus[Parent]): Unit
 
-      //upper bound (B as subtype of A)
-      def set[B <: A](b: B): Box[B] = new Box[B]
-    }
-
-    val parent: Box[Parent] = new Box[Parent]
-
-    //Allowed, because Child is a subtype of Parent
-    val parentToChild: Box[Child] = parent
-    val child: Box[Child] = parent.set(new Child {})
-
-    //Not allowed, because Human is a supertype of Parent
-    //  val parentToHuman: Box[Human] = parent
-    //  val human: Box[Human] = parent.set(new Human {})
+    drive(new Bus[Human])
+    drive(new Bus[Parent])
+    //drive(new Bus[Child])
   }
 
-  object covariance {
+  trait covariance {
 
-    //covariant, can result in type of A and its supertypes
-    class Box[+A] {
+    //covariant - can take type of A and its subtypes
+    class Bus[+A] {}
+    def drive(a: Bus[Parent]): Unit
 
-      //lower bound (B as supertype of A)
-      def set[B >: A](b: B): Box[B] = new Box[B]
-    }
-
-    val parent: Box[Parent] = new Box[Parent]
-
-    //Not allowed, because Child is a subtype of Parent
-    //  val parentToChild: Box[Child] = parent
-    //  val child: Box[Child] = parent.set(new Child {})
-
-    //Allowed, because Human is a supertype of Parent
-    val parentToHuman: Box[Human] = parent
-    val human: Box[Human] = parent.set(new Human {})
-
-    //List is covariant - abstract class List[+A]
-    val parents: List[Parent] = List(new Parent {})
-    val a: List[Parent] = new Child {} :: parents //compiles, because we added a Child which contains Parent properties (Child is a subtype of Parent) AND the result type is Parent (List[+A])
-    val a1: List[Human] = new Child {} :: parents //same as above, plus compiles because we defined result type as supertype of Parent
-//    val b: List[Child] = new Child {} :: parents //can't compile, because we added a Child which contains Parent properties (Child is a subtype of Parent), BUT cannot "cast" to subtype of Parents (List[+A])
-//    val c: List[Parent] = new Human {} :: parents //can't compile, because Human is a supertype of Parent, BUT we cannot "cast" to Parent from Human
-    val d: List[Human] = new Human {} :: parents //compiles, because Human is a supertype of Parent (List[+A]) AND we "cast" to that supertype
+    //drive(new Bus[Human])
+    drive(new Bus[Parent])
+    drive(new Bus[Child])
   }
 }
